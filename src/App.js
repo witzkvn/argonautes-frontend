255f4bc7427cac2cec51dbf7c1593c5680c48e3a
witzkvn <witz.kevin05@gmail.com>
2022-07-08 13:39:02 +0200

    general layout from model in static ok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ArgonauteForm from "./components/argonaute-form/argonaute-form";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h2>Ajouter un(e) Argonaute</h2>
+      <ArgonauteForm />
+      <h2>Membres de l'équipage</h2>
+      <section className="member-list">
+        <div className={styles.memberItem}>Eleftheria</div>
+      </section>
+    </>
   );
 }
 
---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import "./layout.css";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
